4ecc7167f38c223b5390d7a762363ff738299bc8
DominicL3 <leducd98@gmail.com>
2026-09-02 01:11:30 -0700

    Add port override to vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,8 @@ import cesium from 'vite-plugin-cesium'
 export default defineConfig({
   plugins: [vue(), cesium()],
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+  // Fallback to Vite's default port 5173 if PORT is not set
+  server: {
+    port: Number(process.env.PORT) || 5173,
+  }
 })
